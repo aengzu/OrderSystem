6545b6ee9a57bd55f66021118a8210a480129210
thornappl2 <thornappl2@gmail.com>
2023-11-20 19:30:38 +0900

    first commit

--- src/FileHandler.java
+++ src/FileHandler.java
@@ -8,7 +8,7 @@ public class FileHandler {
         this.filePath = filePath;
     }
 
-    // Customer 정보 파일에 저장하기
+    // Customer 정보 파일에 저장
     public void saveCustomerInfo(Customer customer) {
         try {
             FileWriter writer = new FileWriter(filePath, true);
